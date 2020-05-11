76ad521ae2281a65973f16637e3c4d9f26e1d742
David Rubio <davidrubiobenito@gmail.com>
2020-05-11 12:38:00 +0200

    my change title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
+        <h1 className="App-title">Fibonacci Calculator</h1>
         <Switch>
           <div>
             <Nav />
